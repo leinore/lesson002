c80d06d1885b5aa198112318e26ffe6252fb70a2
leinore <116540768+leinore@users.noreply.github.com>
2022-11-18 18:25:20 +0300

    add less_MethodArray

---
+++ less_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 103 ,22 ,3 , 7, 4 ,35 ,64 ,7 ,86 };
+
+int n = array.Length;
+int find = 64;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
